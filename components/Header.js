fbd18ecb52ac8672ac173a26d10d4b6adf4ca39c
Eli Pagan <mbk8689@gmail.com>
2022-03-30 14:44:58 -0400

    added instagram image to header

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,22 @@
+import Image from 'next/image'
+
 function Header() {
   return (
     <div>
-      <h1>I am a header</h1>
+      <div>
+        {/* {left} */}
+        <div className="relative h-24 w-24">
+          <Image
+            layout="fill"
+            src="https://i.pinimg.com/originals/ac/76/72/ac76724616bde8c8749f73c1d07ee255.png"
+          />
         </div>
+
+        {/* {middle} */}
+
+        {/* {right} */}
+      </div>
+    </div>
   )
 }
 export default Header
