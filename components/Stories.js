ce7c8935014a32830254bb48b5f73722bc2857be
Eli Pagan <mbk8689@gmail.com>
2022-04-01 13:53:59 -0400

    usestate to get our fake profiles to render for stories

--- components/Stories.js
+++ components/Stories.js
@@ -1,14 +1,24 @@
 import { faker } from '@faker-js/faker'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 function Stories() {
+  const [suggestions, setSuggestions] = useState([])
+
   useEffect(() => {
     const suggestions = [...Array(25)].map((_, i) => ({
       ...faker.helpers.contextualCard(),
       id: i,
-    }));console.log(suggestions)
+    }))
+
+    setSuggestions(suggestions)
   }, [])
 
-  return <div></div>
+  return (
+    <div>
+      {suggestions.map((profile) => (
+        <Story key="" />
+      ))}
+    </div>
+  )
 }
 export default Stories
