9b5006d78b738e7694d35d4997e331749d9bb7a3
Eli Pagan <mbk8689@gmail.com>
2022-04-03 16:37:29 -0400

    finished styling for stories

--- components/Stories.js
+++ components/Stories.js
@@ -17,7 +17,7 @@ function Stories() {
   return (
     <div
       className="mt-8 flex space-x-2 overflow-x-scroll rounded-sm border
-     border-gray-200 bg-white p-6"
+     border-gray-200 bg-white p-6 scrollbar-thin scrollbar-thumb-black"
     >
       {suggestions.map((profile) => (
         <Story
