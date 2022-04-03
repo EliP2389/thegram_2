829522f2162f761f8b887aa4192f1385bc159bb8
Eli Pagan <mbk8689@gmail.com>
2022-04-03 16:20:38 -0400

    styled image and the name for users Story

--- components/Story.js
+++ components/Story.js
@@ -1,8 +1,13 @@
 function Story({ img, username }) {
   return (
     <div>
-        <img src={img} alt='profile image'/>
-        <p>{username}</p>
+      <img
+        className="h-14 w-14 cursor-pointer rounded-full border-2 border-red-500 
+        object-contain p-[1.5px] hover:scale-110 transition transform duration-200 ease-out"
+        src={img}
+        alt="profile image"
+      />
+      <p className="text-sm w-14 truncate text-center">{username}</p>
     </div>
   )
 }
