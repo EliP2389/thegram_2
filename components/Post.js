dfec5083dcc5f73d090f8ce1fa2d6bd7a94d8a5d
Eli Pagan <mbk8689@gmail.com>
2022-04-04 08:14:53 -0400

    created fake data to map through for posts

---
+++ components/Post.js
@@ -0,0 +1,8 @@
+function Post() {
+  return (
+    <div>
+        <h1>I am a Post</h1>
+    </div>
+  )
+}
+export default Post
--- components/Posts.js
+++ components/Posts.js
@@ -1,8 +1,34 @@
-function Post() {
+import Post from './Post';
+
+const post = [
+  {
+    id: '123',
+    username: 'pagan123',
+    userImg: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQws2ynrnSprthzn-YO-gAvkARHAlON2Yicw&usqp=CAU",
+    img: 'https://links.papareact.com/3ke',
+    caption: 'SUBSCRIBE BECAUSE THIS IS AMAZING!!!'
+  },
+  {
+    id: '123',
+    username: 'pagan123',
+    userImg: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQws2ynrnSprthzn-YO-gAvkARHAlON2Yicw&usqp=CAU",
+    img: 'https://links.papareact.com/3ke',
+    caption: 'SUBSCRIBE BECAUSE THIS IS AMAZING!!!'
+  },
+  {
+    id: '123',
+    username: 'pagan123',
+    userImg: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQws2ynrnSprthzn-YO-gAvkARHAlON2Yicw&usqp=CAU",
+    img: 'https://links.papareact.com/3ke',
+    caption: 'SUBSCRIBE BECAUSE THIS IS AMAZING!!!'
+  }
+];
+
+function Posts() {
   return (
     <div>
-        
+        <Post />
     </div>
   )
 }
-export default Post
+export default Posts
