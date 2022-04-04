4a7f6d5715e86acb878208af04133408dbe325b0
Eli Pagan <mbk8689@gmail.com>
2022-04-04 08:35:05 -0400

    maping through fake data to create and style posts section

--- components/Post.js
+++ components/Post.js
@@ -1,7 +1,32 @@
-function Post() {
+import { 
+    BookmarkIcon, 
+    ChatIcon,
+     DotsHorizontalIcon, 
+     EmojiHappyIcon, 
+     HeartIcon, 
+     PaperAirplaneIcon } from '@heroicons/react/outline';
+
+import { HeartIcon as HeartIconFilled } from '@heroicons/react/solid';
+
+function Post({id, username, userImg, img, caption}) {
   return (
     <div>
-        <h1>I am a Post</h1>
+       {/* {Header} */}
+       <div>
+           <img src={userImg} alt="profile image" />
+           <p>{username}</p>
+           <DotsHorizontalIcon className="h-5" />
+       </div>
+
+       {/* {Img} */}
+
+       {/* {Buttons} */}
+
+       {/* {Cation} */}
+
+       {/* {Comments} */}
+
+       {/* {Input box} */}
     </div>
   )
 }
--- components/Posts.js
+++ components/Posts.js
@@ -1,33 +1,44 @@
-import Post from './Post';
+import Post from './Post'
 
 const post = [
   {
     id: '123',
     username: 'pagan123',
-    userImg: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQws2ynrnSprthzn-YO-gAvkARHAlON2Yicw&usqp=CAU",
+    userImg:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQws2ynrnSprthzn-YO-gAvkARHAlON2Yicw&usqp=CAU',
     img: 'https://links.papareact.com/3ke',
-    caption: 'SUBSCRIBE BECAUSE THIS IS AMAZING!!!'
+    caption: 'SUBSCRIBE BECAUSE THIS IS AMAZING!!!',
   },
   {
-    id: '123',
+    id: '321',
     username: 'pagan123',
-    userImg: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQws2ynrnSprthzn-YO-gAvkARHAlON2Yicw&usqp=CAU",
+    userImg:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQws2ynrnSprthzn-YO-gAvkARHAlON2Yicw&usqp=CAU',
     img: 'https://links.papareact.com/3ke',
-    caption: 'SUBSCRIBE BECAUSE THIS IS AMAZING!!!'
+    caption: 'SUBSCRIBE BECAUSE THIS IS AMAZING!!!',
   },
   {
-    id: '123',
+    id: '456',
     username: 'pagan123',
-    userImg: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQws2ynrnSprthzn-YO-gAvkARHAlON2Yicw&usqp=CAU",
+    userImg:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQws2ynrnSprthzn-YO-gAvkARHAlON2Yicw&usqp=CAU',
     img: 'https://links.papareact.com/3ke',
-    caption: 'SUBSCRIBE BECAUSE THIS IS AMAZING!!!'
-  }
-];
+    caption: 'SUBSCRIBE BECAUSE THIS IS AMAZING!!!',
+  },
+]
 
 function Posts() {
   return (
     <div>
-        <Post />
+      {post.map((post) => (
+        <Post
+          key={post.id}
+          username={post.username}
+          userImg={post.userImg}
+          img={post.img}
+          caption={post.caption}
+        />
+      ))}
     </div>
   )
 }
