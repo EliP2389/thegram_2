d3253e19305de82fd5fe2a0a51957a7a00f935b6
Eli Pagan <mbk8689@gmail.com>
2022-04-04 08:47:43 -0400

    header for post complete and styled

--- components/Post.js
+++ components/Post.js
@@ -10,11 +10,13 @@ import { HeartIcon as HeartIconFilled } from '@heroicons/react/solid';
 
 function Post({id, username, userImg, img, caption}) {
   return (
-    <div>
+    <div className="bg-white my-7 border rounded-sm">
        {/* {Header} */}
-       <div>
-           <img src={userImg} alt="profile image" />
-           <p>{username}</p>
+       <div className="flex items-center p-5">
+           <img 
+           className="rounded-full h-12 w-12 object-contain border p-1 mr-3"
+           src={userImg} alt="profile image" />
+           <p className="flex-1 font-bold">{username}</p>
            <DotsHorizontalIcon className="h-5" />
        </div>
 
