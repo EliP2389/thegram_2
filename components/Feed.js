98bbaaf2a083f9d314348ced2d30b387826e6e46
Eli Pagan <mbk8689@gmail.com>
2022-04-04 21:10:15 -0400

    created a suggestion and miniprofile component and created a img and text in miniprofile with styling

--- components/Feed.js
+++ components/Feed.js
@@ -1,5 +1,7 @@
 import Stories from './Stories'
 import Posts from './Posts'
+import MiniProfile from './MiniProfile'
+import Suggestions from './Suggestions'
 
 function Feed() {
   return (
@@ -7,14 +9,12 @@ function Feed() {
     xl:max-w-6xl mx-auto">
       <section className="col-span-2">
         <Stories />
-        {/* {posts} */}
         <Posts />
       </section>
 
-      <section>
-        {/* {mini profile} */}
-
-        {/* {suggestions} */}
+      <section className='hidden xl:inline-grid md:col-span-1'>
+        <MiniProfile />
+        <Suggestions />
       </section>
     </main>
   )
---
+++ components/MiniProfile.js
@@ -0,0 +1,18 @@
+function MiniProfile() {
+  return (
+    <div className="flex items-center justify-between mt-14 ml-10">
+        <img
+        className="w-16 h-16 rounded-full border p-[2px]"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQws2ynrnSprthzn-YO-gAvkARHAlON2Yicw&usqp=CAU"
+        alt="profile img" />
+
+        <div>
+            <h2>pagan123</h2>
+            <h3>Welcome to Instagram 2.0</h3>
+        </div>
+
+        <button>Sign Out</button>
+    </div>
+  )
+}
+export default MiniProfile
