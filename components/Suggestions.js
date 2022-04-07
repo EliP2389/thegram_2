219872c1355add644b136645e885ab11eebcab09
Eli Pagan <mbk8689@gmail.com>
2022-04-06 21:54:46 -0400

    created useState and useEffect to create fake data

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -1,6 +1,22 @@
+import faker from "@faker-js/faker";
+import { useEffect } from "react";
+
 function Suggestions() {
+const [suggetsions, setSuggestions] = useState([]);
+
+useEffect(() => {
+    const suggestions = [...Array(6)].map((_, i) => ({
+        ...faker.helpers.contextualCard(),
+        id: i
+    }))
+
+    setSuggestions(suggestions);
+}, []);
+
   return (
-    <div></div>
+    <div>
+
+    </div>
   )
 }
 export default Suggestions
