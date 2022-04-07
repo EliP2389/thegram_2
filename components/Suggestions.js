c9e2540295ea57e7e1210dedb4688fa25e9f855d
Eli Pagan <mbk8689@gmail.com>
2022-04-06 22:03:33 -0400

    suggestions text and styling

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -1,21 +1,24 @@
-import faker from "@faker-js/faker";
-import { useEffect } from "react";
+import faker from '@faker-js/faker'
+import { useEffect, useState } from 'react'
 
 function Suggestions() {
-const [suggetsions, setSuggestions] = useState([]);
+  const [suggetsions, setSuggestions] = useState([])
 
-useEffect(() => {
+  useEffect(() => {
     const suggestions = [...Array(6)].map((_, i) => ({
-        ...faker.helpers.contextualCard(),
-        id: i
+      ...faker.helpers.contextualCard(),
+      id: i,
     }))
 
-    setSuggestions(suggestions);
-}, []);
+    setSuggestions(suggestions)
+  }, [])
 
   return (
-    <div>
-
+    <div className="mt-4 ml-10">
+      <div className="flex justify-between text-sm mb-5">
+        <h3 className="text-sm font-bold text-gray-400">Suggestions for you</h3>
+        <button className="text-gray-600 font-semibold">See All</button>
+      </div>
     </div>
   )
 }
