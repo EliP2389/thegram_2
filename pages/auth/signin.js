aa841ee9c4c35120b301398020e5345d09bfa810
Eli Pagan <mbk8689@gmail.com>
2022-04-08 18:26:12 -0400

    styled sign in with google button and added instagram img

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -4,18 +4,25 @@ import Header from '../../components/Header'
 // browser
 function signin({ providers }) {
   return (
-    <div>
-      <>
+    <>
       <Header />
-        {Object.values(providers).map((provider) => (
-          <div key={provider.name}>
-            <button onClick={() => signIn(provider.id)}>
-              Sign in with {provider.name}
-            </button>
-          </div>
-        ))}
-      </>
-    </div>
+      <div>
+        <img className="" src="https://links.papareact.com/ocw" alt="" />
+        <p></p>
+        <div className="mt-40">
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <button
+                className="rounded-lg bg-blue-500 p-3 text-white"
+                onClick={() => signIn(provider.id)}
+              >
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   )
 }
 
