8f5dbff9a7f697a2b933aedf128a05d795e05cc5
Eli Pagan <mbk8689@gmail.com>
2022-04-10 18:41:32 -0400

    tested out the state we created to see if it renders and it worked as intended

--- components/Modal.js
+++ components/Modal.js
@@ -1,6 +1,13 @@
+import { useRecoilState } from "recoil";
+import { modalState } from "../atoms/modalAtom";
+
 function Modal() {
+const [modal, setModal] = useRecoilState(modalState);
+
   return (
-    <div>I am a Modal</div>
+    <div>
+      {modal && <p>THE MODAL IS OPEN</p>}
+    </div>
   )
 }
 export default Modal
