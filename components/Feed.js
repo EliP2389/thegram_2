abb6154caf2d3da60ef4599b5cec80b2f761e4b1
Eli Pagan <mbk8689@gmail.com>
2022-04-10 10:53:27 -0400

    added functionality that if there is no session then styling changes

--- components/Feed.js
+++ components/Feed.js
@@ -8,14 +8,14 @@ function Feed() {
   const { data: session } = useSession()
   return (
     <main
-      className="mx-auto grid grid-cols-1 md:max-w-3xl md:grid-cols-2 
-    xl:max-w-6xl xl:grid-cols-3"
+      className={`mx-auto grid grid-cols-1 md:max-w-3xl md:grid-cols-2 
+    xl:max-w-6xl xl:grid-cols-3 ${ !session && '!grid-cols-1 !max-w-3xl'}`}
     >
       <section className="col-span-2">
         <Stories />
         <Posts />
       </section>
-      
+
       {session && (
         <section className="hidden md:col-span-1 xl:inline-grid">
           <div className="fixed top-20">
