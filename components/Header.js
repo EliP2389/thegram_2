4fcb2bfcc7e6a23ab043792b046e6f62104efd9a
Eli Pagan <mbk8689@gmail.com>
2022-04-10 18:31:07 -0400

    set the piece of state created(modalState) in header.js and added it to be used in PlusCircleIcon

--- components/Header.js
+++ components/Header.js
@@ -10,9 +10,12 @@ import {
 import { HomeIcon } from '@heroicons/react/solid'
 import { signOut, signIn, useSession } from 'next-auth/react'
 import { useRouter } from 'next/router';
+import { useRecoilState } from 'recoil';
+import { modalState } from '../atoms/modalAtom';
 
 function Header() {
   const { data: session } = useSession();
+  const [modal, setModal] = useRecoilState(modalState);
   const router = useRouter();
   console.log(session)
 
@@ -68,7 +71,7 @@ function Header() {
                 </div>
               </div>
 
-              <PlusCircleIcon className="navBtn" />
+              <PlusCircleIcon onClick={() => {setModal(true)}} className="navBtn" />
               <UserGroupIcon className="navBtn" />
               <HeartIcon className="navBtn" />
 
