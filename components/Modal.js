2cb906a899f7792e73179e3b25142d225dc10e51
Eli Pagan <mbk8689@gmail.com>
2022-04-12 17:47:58 -0400

    added Transition.Root and Dialog to our Modal.js for the start of rendering a add post modal

--- components/Modal.js
+++ components/Modal.js
@@ -1,14 +1,21 @@
-import { useRecoilState } from "recoil";
-import { modalState } from "../atoms/modalAtom";
-import { Dialog, Transition } from "react";
+import { useRecoilState } from 'recoil'
+import { modalState } from '../atoms/modalAtom'
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment } from 'react'
 
 function Modal() {
-const [modal, setModal] = useRecoilState(modalState);
+  const [modal, setModal] = useRecoilState(modalState)
 
   return (
-    <div>
-      <h1></h1>
-    </div>
+    <Transition.Root show={true} as={Fragment}>
+      <Dialog
+        as="div"
+        className="fixed inset-0 z-10 overflow-y-auto"
+        onClose={setModal}
+      >
+        <h1>Hello World</h1>
+      </Dialog>
+    </Transition.Root>
   )
 }
 export default Modal
