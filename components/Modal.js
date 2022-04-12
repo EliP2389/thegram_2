edcede43b9b007c20f7be232aa59de67c5ae6dc4
Eli Pagan <mbk8689@gmail.com>
2022-04-12 17:41:50 -0400

    imported Dialog and Transition

--- components/Modal.js
+++ components/Modal.js
@@ -1,12 +1,13 @@
 import { useRecoilState } from "recoil";
 import { modalState } from "../atoms/modalAtom";
+import { Dialog, Transition } from "react";
 
 function Modal() {
 const [modal, setModal] = useRecoilState(modalState);
 
   return (
     <div>
-      {modal && <p>THE MODAL IS OPEN</p>}
+      <h1></h1>
     </div>
   )
 }
