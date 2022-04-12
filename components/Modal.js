c9a1785c17f47d86bc63c6c2134c8e4b1e14db31
Eli Pagan <mbk8689@gmail.com>
2022-04-12 17:57:24 -0400

    added Transition.Child and styled it. This part is what renders the content to be shown

--- components/Modal.js
+++ components/Modal.js
@@ -13,7 +13,22 @@ function Modal() {
         className="fixed inset-0 z-10 overflow-y-auto"
         onClose={setModal}
       >
-        <h1>Hello World</h1>
+        <div
+          className="flex min-h-[800px] items-end justify-center 
+        px-4 pt-4 pb-20 text-center sm:block sm:min-h-screen sm:p-0"
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in  duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+              
+          </Transition.Child>
+        </div>
       </Dialog>
     </Transition.Root>
   )
