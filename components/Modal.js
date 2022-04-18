641d169bed700f9c1c3c74426fb3f0a2e901ea5c
Eli Pagan <mbk8689@gmail.com>
2022-04-18 18:55:42 -0400

    fixed errors and now able to upload posts in the DATABASE

--- components/Modal.js
+++ components/Modal.js
@@ -10,29 +10,29 @@ import {
   doc,
   serverTimestamp,
   updateDoc,
-} from 'firebase/firestore'
+} from '@firebase/firestore'
 import { useSession } from 'next-auth/react'
-import { ref, getDownloadURL, uploadString } from 'firebase/storage'
+import { ref, getDownloadURL, uploadString } from '@firebase/storage'
 
 function Modal() {
-  const { data: session } = useSession()
-  const [modal, setModal] = useRecoilState(modalState)
-  const filePickerRef = useRef(null)
-  const captionRef = useRef(null)
-  const [loading, setLoading] = useState(false)
-  const [selectedFile, setSelectedFile] = useState(null)
+  const { data: session } = useSession();
+  const [modal, setModal] = useRecoilState(modalState);
+  const filePickerRef = useRef(null);
+  const captionRef = useRef(null);
+  const [loading, setLoading] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(null);
 
   const uploadPost = async () => {
-    if (loading) return
+    if (loading) return;
 
-    setLoading(true)
+    setLoading(true);
 
     // 1) create a post and add to firestore 'posts' collection
     // 2) get the post ID for the newly created posts
     // 3) upload the image to firebase storage with the post ID
     // 4) get a download URL from firebase storage and update the original post with an image
 
-    const docRef = await addDoc(collection(db, 'posts'), {
+    const docRef = await addDoc(collection(db, "posts"), {
       username: session.user.username,
       caption: captionRef.current.value,
       profileImg: session.user.image,
@@ -162,12 +162,14 @@ function Modal() {
                 <div className="mt-5 sm:mt-6">
                   <button
                     type="button"
+                    disabled={!selectedFile}
+                    onClick={uploadPost}
                     className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600
                      px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none
                      focus:ring-2 focus:ring-red-500 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-gray-300
                      hover:disabled:bg-gray-300 sm:text-sm"
                   >
-                    Upload Post
+                    {loading ? 'Uploading...' : 'Upload Post'}
                   </button>
                 </div>
               </div>
