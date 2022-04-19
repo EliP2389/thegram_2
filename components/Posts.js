220f21334f07f743cc6f1653dd5e9410094cb528
Eli Pagan <mbk8689@gmail.com>
2022-04-19 18:11:23 -0400

    posting functionality works

--- components/Posts.js
+++ components/Posts.js
@@ -1,50 +1,34 @@
-import { collection, onSnapshot, query } from 'firebase/firestore';
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
 import { useEffect, useState } from 'react'
+import { db } from '../firebase'
 import Post from './Post'
 
-const post = [
-  {
-    id: '123',
-    username: 'pagan123',
-    userImg:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQws2ynrnSprthzn-YO-gAvkARHAlON2Yicw&usqp=CAU',
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQws2ynrnSprthzn-YO-gAvkARHAlON2Yicw&usqp=CAU',
-    caption: 'SUBSCRIBE BECAUSE THIS IS AMAZING!!!',
-  },
-  {
-    id: '321',
-    username: 'pagan123',
-    userImg:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQws2ynrnSprthzn-YO-gAvkARHAlON2Yicw&usqp=CAU',
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQws2ynrnSprthzn-YO-gAvkARHAlON2Yicw&usqp=CAU',
-    caption: 'SUBSCRIBE BECAUSE THIS IS AMAZING!!!',
-  },
-  {
-    id: '456',
-    username: 'pagan123',
-    userImg:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQws2ynrnSprthzn-YO-gAvkARHAlON2Yicw&usqp=CAU',
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQws2ynrnSprthzn-YO-gAvkARHAlON2Yicw&usqp=CAU',
-    caption: 'SUBSCRIBE BECAUSE THIS IS AMAZING!!!',
-  },
-]
-
 function Posts() {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([])
 
-  useEffect(() => {
-   onSnapshot(collection(query(db, 'posts')))
-  }, [])
+  useEffect(
+    () =>
+      // snapshot is a real time listener for the backend
+      onSnapshot(
+        query(collection(db, 'posts'), orderBy('timestamp', 'desc')),
+        (snapshot) => {
+          setPosts(snapshot.docs)
+        }
+      ),
+    [db]
+  );
 
+console.log(posts);
   return (
     <div>
-      {post.map((post) => (
+      {posts.map((post) => (
         <Post
           key={post.id}
-          username={post.username}
-          userImg={post.userImg}
-          img={post.img}
-          caption={post.caption}
+          id={post.id}
+          username={post.data().username}
+          userImg={post.data().profileImg}
+          img={post.data().image}
+          caption={post.data().caption}
         />
       ))}
     </div>
