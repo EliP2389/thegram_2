e0a88d4600f0c5520bdabeea5772dee02b84297c
Eli Pagan <mbk8689@gmail.com>
2022-04-20 16:52:51 -0400

    set up timestamp functionality using Moment

--- components/Post.js
+++ components/Post.js
@@ -100,6 +100,9 @@ function Post({ id, username, userImg, img, caption }) {
                 <span className="font-bold">{comment.data().username}</span>{' '}
                 {comment.data().comment}
               </p>
+              <Moment fromNow className="pr-5 text-xs">
+                {comment.data().timestamp?.toDate()}
+              </Moment>
             </div>
           ))}
         </div>
